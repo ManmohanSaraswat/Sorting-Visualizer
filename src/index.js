a6f78e84ac0a1f959ea00a83af11810caec38b17
Manmohan Saraswat <40047862+ManmohanSaraswat@users.noreply.github.com>
2024-11-03 09:09:41 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './front.css'
+import Chart from './Chart'
+
+ReactDOM.render(
+    <>
+        <Chart/>
+    </>,
+    document.getElementById('root')
+);
